e35444d642d79d52f48c724b92f3329a332cf8b4
Владислав П <sys.system@mail.ru>
2015-03-05 23:35:00 +0500

    MVC Updated

--- system/config/mysql.conf.php
+++ system/config/mysql.conf.php
@@ -1,13 +1,5 @@
 <?php
 
-define('MYSQL_CONNECTION',mysql_connect('localhost','root','')); // Подключение к БД
-
-mysql_select_db('tm'); // Выбор Базы
-
-// Установка режима кодировки
-
-//mysql_query("set character_set_connection=cp1251;",MYSQL_CONNECTION);
-
-//mysql_query("set character_set_client=utf8;",MYSQL_CONNECTION);
-
-//mysql_query("set character_set_results=utf8;",MYSQL_CONNECTION);
+define('MYSQL_SERVER','localhost');
+define('MYSQL_USER','root');
+define('MYSQL_PASSWORD','');
---
+++ templates/default/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Титл</title>
+    <meta charset="utf-8" />
+    <link href="<?=DIR_TMPL?>styles/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <header>
+        <ul>
+            <li>
+                <a href="/">Главная</a>
+            </li>
+            <li>
+                <a href="index.php">Задачи</a>
+            </li>
+            <li>
+                <a href="index.php">Проекты</a>
+            </li>
+            <li>
+                <a href="index.php">Группы</a>
+            </li>
+        </ul>
+        <div class="user_menu">
+            Александр Ларионов
+            <ul>
+                <li>
+                    <a>Мои данные</a>
+                </li>
+                <li>
+                    <a>Настройки</a>
+                </li>
+                <li>
+                    <a>Выход</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+    <div>
+        gregrg
+    </div>
+</body>
+</html>
